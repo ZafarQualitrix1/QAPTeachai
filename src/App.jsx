25abeb1111db2259c48ba164d21f90cf055768e5
Qualitrix <zafar.alam@qualitrix.com>
2026-02-27 00:17:05 +0530

    Add image to dummy page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div>
       <h1>Dummy Page</h1>
       <p>This is a placeholder page for launch purposes.</p>
+      <img src="/vite.svg" alt="Launch Image" style={{ width: '100px', height: '100px' }} />
     </div>
   );
 }
